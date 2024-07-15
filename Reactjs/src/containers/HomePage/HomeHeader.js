e5a5881aec0352abdac402e104b106f3d0b87e90
haquochuu <huu3675@gmail.com>
2024-07-16 00:31:32 +0700

    finish #45

--- Reactjs/src/containers/HomePage/HomeHeader.js
+++ Reactjs/src/containers/HomePage/HomeHeader.js
@@ -7,7 +7,7 @@ class HomeHeader extends Component {
     render() {
         return (
             <>
-                <div className='home-header-container'>
+                <div className='home-header-container red'>
                     <div className='home-header-content red'>
                         <div className='left-content brown'>
                             <i className="fas fa-bars"></i>
@@ -36,6 +36,71 @@ class HomeHeader extends Component {
                             <span className='help-text'>Hỗ trợ</span>
                             <span>|</span>
                             <div className='language'>VN</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='home-header-banner red'>
+                    <div className='content-up red'>
+                        <div className='text1 red'>
+                            <span> Nền tảng y tế</span>
+                        </div>
+                        <div className='text2 red'>
+                            <span> chăm sóc sức khỏe toàn diện</span>
+                        </div>
+                        <div className='search'>
+                            <i className="fas fa-search"></i>
+                            <input className='search-input' placeholder='Tìm gói khám' />
+                        </div>
+                    </div>
+                    <div className='content-down green'>
+                        <div className='child-content'>
+                            <div className='child-icon'>
+                                <i class="fas fa-hospital"></i>
+                            </div>
+                            <div className='subscript'>
+                                Khám chuyên khoa
+                            </div>
+                        </div>
+                        <div className='child-content'>
+                            <div className='child-icon'>
+                                <i class="fas fa-phone"></i>
+                            </div>
+                            <div className='subscript'>
+                                Khám <br />từ xa
+                            </div>
+                        </div>
+                        <div className='child-content'>
+                            <div className='child-icon'>
+                                <i class="fas fa-hospital-alt"></i>
+                            </div>
+                            <div className='subscript'>
+                                Khám tổng quát
+                            </div>
+                        </div>
+                        <div className='child-content'>
+                            <div className='child-icon'>
+                                <i class="fas fa-vial"></i>
+                            </div>
+                            <div className='subscript'>
+                                Xét nghiệm y học
+                            </div>
+                        </div>
+                        <div className='child-content'>
+                            <div className='child-icon'>
+                                <i class="fas fa-skull"></i>
+                            </div>
+                            <div className='subscript'>
+                                Sức khỏe tinh thần
+                            </div>
+                        </div>
+                        <div className='child-content'>
+                            <div className='child-icon'>
+                                <i class="fas fa-user-md"></i>
+                            </div>
+                            <div className='subscript'>
+                                Khám nha khoa
+                            </div>
                         </div>
                     </div>
                 </div>
